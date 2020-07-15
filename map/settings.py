971f777393d4ad6be04b758ce055c3d814aff1f6
jack <la5c@qq.com>
2020-07-15 22:30:41 +0800

    update settings

--- map/settings.py
+++ map/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = '/statics'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 LOCAL_STATIC = False
 
