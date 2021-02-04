ec29a86e7dcae51bf857bbcae26461a940be45f2
FreeYeti <yeti@freeyeti.net>
2021-02-04 16:18:46 +0800

    updated reporting script

--- report.py
+++ report.py
@@ -3,7 +3,7 @@ from django.core.management import execute_from_command_line
 
 cov = coverage.Coverage()
 cov.start()
-execute_from_command_line(['manage.py', 'test', 'geodata'])
+execute_from_command_line(['manage.py', 'test', '--noinput', 'geodata'])
 cov.stop()
 cov.save()
 cov.report()
